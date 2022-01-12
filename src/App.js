05feeea6e018e0027a3e3731db2e9b4c0c75ea84
jimsam1 <dimitris.samras@gmail.com>
2022-01-12 03:15:22 +0200

    2o_erwthma

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-	const [ data, setData ] = useState('');
-	const [ query, setQuery ] = useState('');
-	const [ sponsorName, setSponsorName ] = useState('');
-	const [ year, setYear ] = useState('');
-	const [ season, setSeason ] = useState('');
-	const [ team, setTeam ] = useState('');
-	const [ error, setError ] = useState(false);
-	const [ message, setMessage ] = useState('');
-	const [ url, setUrl ] = useState('');
+	const [data, setData] = useState('');
+	const [query, setQuery] = useState('');
+	const [sponsorName, setSponsorName] = useState('');
+	const [year, setYear] = useState('');
+	const [season, setSeason] = useState('');
+	const [team, setTeam] = useState('');
+	const [error, setError] = useState(false);
+	const [message, setMessage] = useState('');
+	const [stadium, setStadium] = useState('');
+	const [date, setDate] = useState('');
+	const [url, setUrl] = useState('');
 	const baseUrl = 'http://localhost:8084';
 
 	const postDataSponsor = () => {
@@ -43,9 +45,30 @@ function App() {
 			.then((data) => setData(data));
 	};
 
+	const postDataSportAtStadium = () => {
+		if (!date || !stadium ) {
+			setError(true);
+			setMessage('Please fill all the fields');
+		}
+		const requestOptions = {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' },
+			//body: JSON.stringify({ stadium, date})
+		};
+		fetch(baseUrl + '/sport/search' , requestOptions)
+			.then((response) => response.json())
+			.then((data) => setData(data));
+			console.log(data,"TELOS EDW")
+	};
+	
+
+
 	return (
 		<div className="App">
+
 			<h3>Olympics App</h3>
+
+
 			<input type="text" onChange={(e) => setTeam(e.target.value)} placeholder="Search By Team" />
 			<button onClick={postDataAthlete}>search</button>
 			{data &&
@@ -57,6 +80,8 @@ function App() {
 					</div>
 				))}
 			{error && <div style={{ color: 'red' }}>{message}</div>}
+
+
 			<form>
 				<input
 					type="text"
@@ -64,10 +89,44 @@ function App() {
 					placeholder="Sponsor Name"
 					required
 				/>
-				<input type="text" onChange={(e) => setYear(e.target.value)} placeholder="Year" required />
-				<input type="text" onChange={(e) => setSeason(e.target.value)} placeholder="Season" required />
+				<input
+					type="text"
+					onChange={(e) => setYear(e.target.value)}
+					placeholder="Year"
+					required
+				/>
+				<input
+					type="text"
+					onChange={(e) => setSeason(e.target.value)}
+					placeholder="Season"
+					required
+				/>
 				<button onClick={postDataSponsor}>Send Request</button>
 			</form>
+
+			<form>
+				<input
+					type="text"
+					onChange={(e) => setStadium(e.target.value)}
+					placeholder="Stadium"
+					required
+				/>
+				<input
+					type="text"
+					onChange={(e) => setDate(e.target.value)}
+					placeholder="Date"
+					required
+				/>
+				<button onClick={postDataSportAtStadium}>Search for Sports</button>
+			</form>
+			{data &&
+				data.map((item) => (
+					<div >
+						<div>
+							{item.sportName} {item.stadiumName}
+						</div>
+					</div>
+				))}
 		</div>
 	);
 }
